a75ca03e04d068edf05115fae59b932877169d6c
MallikarjunH <Mallikarjun.h1410@gmail.com>
2020-03-09 16:14:46 +0530

    Added actions

--- AddGuestVisitor_UI/AddGuestVisitor_UI/ViewController.swift
+++ AddGuestVisitor_UI/AddGuestVisitor_UI/ViewController.swift
@@ -17,14 +17,66 @@ class ViewController: UIViewController {
     @IBOutlet weak var tableViewHeightConstraint: NSLayoutConstraint!
     
     
+    @IBOutlet weak var occasionalyView: UIView!
+    @IBOutlet weak var weeklyView: UIView!
+    @IBOutlet weak var dailyView: UIView!
+    @IBOutlet weak var saveImage: UIImageView!
+    
+    var gesture:UITapGestureRecognizer!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        
         self.tableViewHeightConstraint.constant = CGFloat((72*contactsNumber)) // (72 * 4)
+        
+        //save button
+        gesture = UITapGestureRecognizer(target: self, action: #selector(didTapedOnSaveButton(tapGestureRecognizer:)))
+        saveImage.isUserInteractionEnabled = true
+        saveImage.addGestureRecognizer(gesture)
+        //Occasionaly
+        gesture = UITapGestureRecognizer(target: self, action:#selector (didTapOnAddVisitorOccasionaly))
+        occasionalyView.isUserInteractionEnabled = true
+        occasionalyView.addGestureRecognizer(gesture)
+        
+        //weekly
+        gesture = UITapGestureRecognizer(target: self, action:#selector (didTapOnAddVisitorWeekly))
+        weeklyView.isUserInteractionEnabled = true
+        weeklyView.addGestureRecognizer(gesture)
+    
+        //Daily
+        gesture = UITapGestureRecognizer(target: self, action:#selector (didTapOnAddVisitorDaily))
+        dailyView.isUserInteractionEnabled = true
+        dailyView.addGestureRecognizer(gesture)
     }
 
+    
+    @objc func didTapOnAddVisitorOccasionaly(tapGestureRecognizer: UITapGestureRecognizer) {
+    
+        print("Clicked on Add Visitor Occasionaly")
+    }
+
+
+    @objc func didTapedOnSaveButton(tapGestureRecognizer: UITapGestureRecognizer) {
+        print("Clicked on Add Save Button")
+    
+    }
+    
+    @objc func didTapOnAddVisitorWeekly(tapGestureRecognizer: UITapGestureRecognizer) {
+        
+         print("Clicked on Add Visitor Weekly")
+    }
+    
+    @objc func didTapOnAddVisitorDaily(tapGestureRecognizer: UITapGestureRecognizer) {
+         print("Clicked on Add Visitor Daily")
+        
+    }
+    
+    @IBAction func didTapOnAddMoreVisitorsButton(_ sender: Any) {
+        print("Clicked on Add More Visitors")
+    }
+    
 
 }
 
@@ -48,6 +100,10 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         return 72.0
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        print("Clicked on Index: \(indexPath.row)")
+    }
     
     
 }
