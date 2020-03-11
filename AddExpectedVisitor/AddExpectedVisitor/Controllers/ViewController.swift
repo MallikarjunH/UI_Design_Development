57001c49cd8077f90790f6e23d7f1efd5dfe7cc4
MallikarjunH <mallikarjun.h1410@gmail.com>
2020-03-11 19:14:01 +0530

    dynamic textView test

--- AddExpectedVisitor/AddExpectedVisitor/Controllers/ViewController.swift
+++ AddExpectedVisitor/AddExpectedVisitor/Controllers/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
 
+    
+    @IBOutlet weak var textViewOutlet: UITextView!
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        textViewOutlet.text = "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+      // textViewOutlet.translatesAutoresizingMaskIntoConstraints = true
+        
+        textViewOutlet.translatesAutoresizingMaskIntoConstraints = false
+        //textViewOutlet.isScrollEnabled = false
+        
     }
 
 
+   func textViewDidChange(_ textView: UITextView) {
+        let fixedWidth = textView.frame.size.width
+        textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+        let newSize = textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+        var newFrame = textView.frame
+        newFrame.size = CGSize(width: max(newSize.width, fixedWidth), height: newSize.height)
+        textView.frame = newFrame
+    } 
 }
 
